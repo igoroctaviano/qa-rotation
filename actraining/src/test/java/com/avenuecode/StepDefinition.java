03eed7ba36eaed2d3c97be41dd90e6eabba22edc
igoroctaviano <igoroctaviano@gmail.com>
2017-10-16 11:26:18 -0200

    Update yml

--- actraining/src/test/java/com/avenuecode/StepDefinition.java
+++ actraining/src/test/java/com/avenuecode/StepDefinition.java
@@ -10,18 +10,18 @@ public abstract class StepDefinition {
     protected static WebDriver driver;
 
     protected WebDriver getDriver() {
-        // System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver");
-        final ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setBinary(System.getProperty("user.dir") + "/chromedriver");
-        chromeOptions.addArguments("--headless");
-        chromeOptions.addArguments("--disable-gpu");
-
-        final DesiredCapabilities dc = new DesiredCapabilities();
-        dc.setJavascriptEnabled(true);
-        dc.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-
         if (driver == null) {
-            driver = new ChromeDriver();
+            // System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver");
+            final ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.setBinary(System.getProperty("user.dir") + "/chromedriver");
+            chromeOptions.addArguments("--headless");
+            chromeOptions.addArguments("--disable-gpu");
+
+            final DesiredCapabilities dc = new DesiredCapabilities();
+            dc.setJavascriptEnabled(true);
+            dc.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+
+            driver = new ChromeDriver(dc);
         }
         return driver;
     }
